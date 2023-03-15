b2f3809c8d06b9e4eadb4de88127ec7d765079c6
Francesco Monti <58150610+FrancescoMonti-source@users.noreply.github.com>
2023-03-15 14:18:51 +0100

    yuyutut

--- R/t_col.R
+++ R/t_col.R
@@ -1,7 +1,3 @@
-## Transparent colors
-## Mark Gardener 2015
-## www.dataanalytics.org.uk
-
 t_col <- function(color, percent = 50, name = NULL) {
     #      color = color name
     #    percent = % transparency
@@ -12,11 +8,14 @@ t_col <- function(color, percent = 50, name = NULL) {
 
     ## Make new color using input color as base and alpha set by transparency
     t.col <- rgb(rgb.val[1], rgb.val[2], rgb.val[3],
-                 max = 255,
+                 maxColorValue = 255,
                  alpha = (100 - percent) * 255 / 100,
                  names = name)
 
     ## Save the color
     invisible(t.col)
 }
-## END
+# END
+# Transparent colors
+# Mark Gardener 2015
+# www.dataanalytics.org.uk
