670298aee4f188e5c22c5a5db95459d0b2b35eac
FrancescoMonti-source <58150610+FrancescoMonti-source@users.noreply.github.com>
2023-08-05 11:17:24 +0200

    New function: save_editable_plot
    Add function for saving ggplot2 plots as editable PowerPoint slides
    
    This commit introduces the `save_editable_plot` function, which saves ggplot2 plots as editable graphics in new PowerPoint files. The function checks if the necessary packages ("ggplot2", "officer", "rvg") are installed and, if not, prompts the user to install them. This function will be useful for creating editable visuals for reports and presentations.

---
+++ R/save_editable_plot.R
@@ -0,0 +1,54 @@
+#' Save an Editable ggplot2 Plot as a PowerPoint Slide
+#'
+#' This function takes a ggplot2 plot and saves it as an editable graphic in a new PowerPoint file.
+#' The function checks if the necessary packages ("ggplot2", "officer", "rvg") are installed,
+#' and if not, prompts the user to install them.
+#'
+#' @param plot The ggplot2 plot to be saved.
+#' @param filename A character string specifying the name of the PowerPoint file to be created (without the .pptx extension).
+#' @param path A character string specifying the directory where the PowerPoint file should be saved. Defaults to the current working directory.
+#'
+#' @return A PowerPoint file containing the ggplot2 plot as an editable graphic is saved to the specified directory.
+#'
+#' @examples
+#' \dontrun{
+#' p <- ggplot(mtcars, aes(mpg, hp)) +
+#'   geom_point() +
+#'   theme_minimal()
+#' save_editable_plot(p, filename = "my_plot", path = "your_project_name/graphs")
+#' }
+#'
+#' @export
+
+save_editable_plot <- function(plot, filename, path = "your_path_to_save_files") {
+
+    # List of necessary packages
+    necessary_packages <- c("ggplot2", "officer", "rvg")
+
+    # Check if packages are installed
+    for (pkg in necessary_packages) {
+        if (!require(pkg, character.only = TRUE)) {
+            print(paste(pkg, "not found. Do you want to install it? (y/n)"))
+
+            # Ask for user input
+            answer <- readline(prompt = "Enter y or n: ")
+
+            # If user input is "y", install the package
+            if (answer == "y") {
+                install.packages(pkg)
+                library(pkg, character.only = TRUE)
+            }
+        }
+    }
+
+    # Create a new PowerPoint document using officer
+    doc <- read_pptx()
+
+    # Add a slide and then add the ggplot to the slide
+    doc <- doc %>%
+        add_slide(layout = "Title and Content", master = "Office Theme") %>%
+        ph_with(value = dml(code = print(plot)), location = ph_location_type(type = "body"))
+
+    # Save the PowerPoint document
+    print(doc, target = paste0(path, "/", filename, ".pptx"))
+}
