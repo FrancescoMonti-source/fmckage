e0c198133e7e86ca54679d2c85358b1d20e58ca9
Francesco Monti <58150610+FrancescoMonti-source@users.noreply.github.com>
2023-12-13 18:00:30 +0100

    upload stay_duration()

---
+++ R/stay_duration.R
@@ -0,0 +1,35 @@
+#' Calculate Total Stay Duration
+#'
+#' Computes the total duration of stays in a hospital or similar settings,
+#' accounting for overlapping and non-overlapping intervals.
+#'
+#' @param data A dataframe containing the stay data.
+#' @param id_col The name of the column in `data` that identifies each stay event or patient.
+#' @param start_col The name of the column in `data` representing the start date of each stay.
+#' @param end_col The name of the column in `data` representing the end date of each stay.
+#' @return A dataframe with each unique identifier from `id_col` and the corresponding
+#'         total duration of stays.
+#' @export
+#' @examples
+#' # Assuming 'pmsi_general' is your dataframe and it has columns 'evtid' (event ID),
+#' # 'datent' (admission date), and 'datsort' (discharge date):
+#' # total_durations <- stay_duration(pmsi_general, 'evtid', 'datent', 'datsort')
+#'
+#' @import dplyr
+#' @import purrr
+#' @import lubridate
+stay_duration <- function(data, id_col, start_col, end_col) {
+    # function body
+    stay_duration <- function(data, id_col, start_col, end_col) {
+        data %>%
+            mutate(
+                start = as.numeric(as.Date(!!sym(start_col))),
+                end = as.numeric(as.Date(!!sym(end_col)))
+            ) %>%
+            filter(!is.na(start), !is.na(end)) %>%
+            group_by(!!sym(id_col)) %>%
+            summarise(sequences = list(map2(start, end, seq)), .groups = 'keep') %>%
+            mutate(unioned = map(sequences, ~ reduce(.x, union))) %>%
+            summarise(total_duration = length(unique(unlist(unioned[1]))))
+    }
+}
