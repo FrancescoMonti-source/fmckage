1db98d17a1600b098e6d207b0ca513a4fbcb14a7
Francesco Monti <58150610+FrancescoMonti-source@users.noreply.github.com>
2025-02-07 16:59:06 +0100

    improved column naming in the output

--- R/process_prescriptions.R
+++ R/process_prescriptions.R
@@ -3,7 +3,8 @@ process_prescriptions = function(data){
     data = data %>%
         map_dfr(as.data.frame) %>%
         mutate(across(everything(), function(x) as.character(x))) %>%
-        select(PATID,EVTID,ELTID,everything())
+        select(PATID,EVTID,ELTID,everything()) %>%
+        rename_with(~ str_remove(.x, "^PRES\\."), starts_with("PRES."))
 
     return(data)
 }
