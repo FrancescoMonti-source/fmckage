1b2f9496f8d45854089463323b5a346e515e7362
Francesco Monti <58150610+FrancescoMonti-source@users.noreply.github.com>
2025-03-20 14:44:08 +0100

    hjk

--- R/process_prescriptions.R
+++ R/process_prescriptions.R
@@ -4,7 +4,11 @@ process_prescriptions = function(data){
         map_dfr(as.data.frame) %>%
         mutate(across(everything(), function(x) as.character(x))) %>%
         select(PATID,EVTID,ELTID,everything()) %>%
-        rename_with(~ str_remove(.x, "^PRES\\."), starts_with("PRES."))
+        rename_with(~ str_remove(.x, "^PRES\\."), starts_with("PRES.")) %>%
+        mutate(adm_start = str_extract(CAREPLAN,"[\\d\\-]+"),
+               adm_end = str_extract(CAREPLAN,"[\\d\\-]+ à ([\\d\\-]+)", group = 1)) %>%
+        select(PATID:ELTID,SPELABEL,CAREPLAN,DATENT,DATSORT,DATPRES,adm_start,adm_end, everything()) %>%
+        mutate(across(DATENT:adm_end, function(x) as_date(x)))
 
     return(data)
 }
