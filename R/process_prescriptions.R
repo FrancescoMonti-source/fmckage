5a3d5f5b15b7191325210d91db6c78cfc5393c98
Francesco Monti <58150610+FrancescoMonti-source@users.noreply.github.com>
2025-06-24 16:28:00 +0200

    process_prescriptions() : adm_start and adm_end pattern update

--- R/process_prescriptions.R
+++ R/process_prescriptions.R
@@ -5,8 +5,9 @@ process_prescriptions = function(data){
         mutate(across(everything(), function(x) as.character(x))) %>%
         select(PATID,EVTID,ELTID,everything()) %>%
         rename_with(~ str_remove(.x, "^PRES\\."), starts_with("PRES.")) %>%
-        mutate(adm_start = str_extract(CAREPLAN,"[\\d\\-]+"),
-               adm_end = str_extract(CAREPLAN,"[\\d\\-]+ à ([\\d\\-]+)", group = 1)) %>%
+        mutate(adm_start = str_extract(CAREPLAN, "(\\d{4}\\-\\d{2}\\-\\d{2})\\sà", group = 1),
+               adm_end = str_extract(CAREPLAN, "\\d{4}\\-\\d{2}\\-\\d{2}\\sà\\s(\\d{4}\\-\\d{2}\\-\\d{2})", group = 1)
+               ) %>%
         select(PATID:ELTID,SPELABEL,CAREPLAN,DATENT,DATSORT,DATPRES,adm_start,adm_end, everything()) %>%
         mutate(across(DATENT:adm_end, function(x) as_date(x)))
 
