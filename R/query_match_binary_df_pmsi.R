d2724632b399ee22ca1c4729641c4ee5f4f89170
Francesco Monti <58150610+FrancescoMonti-source@users.noreply.github.com>
2025-09-18 17:41:24 +0200

    make_periods()

--- R/query_match_binary_df_pmsi.R
+++ R/query_match_binary_df_pmsi.R
@@ -53,63 +53,61 @@ query_match_binary_df_pmsi <- function(query_list = NULL,
                                        NOMENCLATURE = "",
                                        UFPRO = "",
                                        UFDEM = "") {
-  results <- purrr::map_dfr(names(query_list), function(query_name) {
-    query <- query_list[[query_name]]
 
-    filters <- list(
-      DALL = query,
-      PATBD = PATBD,
-      PATID = PATID,
-      EVTID = EVTID,
-      ELTID = ELTID,
-      DATENT = DATENT,
-      DATSORT = DATSORT,
-      MODEENT = MODEENT,
-      MODESORT = MODESORT,
-      PMSISTATUT = PMSISTATUT,
-      PATAGE = PATAGE,
-      PATSEX = PATSEX,
-      SEJUM = SEJUM,
-      SEJUF = SEJUF,
-      DATEACTE = DATEACTE,
-      CODEACTE = CODEACTE,
-      NOMENCLATURE = NOMENCLATURE,
-      UFPRO = UFPRO,
-      UFDEM = UFDEM
-    )
+    results <- purrr::map_dfr(names(query_list), function(query_name) {
+        query <- query_list[[query_name]]
 
-    # Remove elements with empty string values
-    filters <- Filter(function(x) x != "", filters)
+        filters <- list(
+            DALL = query,
+            PATBD = PATBD,
+            PATID = PATID,
+            EVTID = EVTID,
+            ELTID = ELTID,
+            DATENT = DATENT,
+            DATSORT = DATSORT,
+            MODEENT = MODEENT,
+            MODESORT = MODESORT,
+            PMSISTATUT = PMSISTATUT,
+            PATAGE = PATAGE,
+            PATSEX = PATSEX,
+            SEJUM = SEJUM,
+            SEJUF = SEJUF,
+            DATEACTE = DATEACTE,
+            CODEACTE = CODEACTE,
+            NOMENCLATURE = NOMENCLATURE,
+            UFPRO = UFPRO,
+            UFDEM = UFDEM
+        )
 
-    json_query <- jsonlite::toJSON(filters, auto_unbox = TRUE)
+        # Remove elements with empty string values
+        filters <- Filter(function(x) x != "", filters)
 
-    # print(json_query)
+        json_query <- jsonlite::toJSON(filters, auto_unbox = TRUE)
 
-    res <- d2imr::d2im_wsc.get_edsan_idtriplets_as_dataframe("pmsi", json_query, "edsan")
+        #print(json_query)
 
-    if (is.null(res) || length(res) == 0) {
-      return(NULL)
-    }
+        res <- d2imr::d2im_wsc.get_edsan_idtriplets_as_dataframe("pmsi", json_query, "edsan")
 
-    res <- tryCatch(
-      {
+        if (is.null(res) || length(res) == 0) {
+            return(NULL)
+        }
+
+        res <- tryCatch({
+            res %>%
+                apply(2, unlist) %>%
+                as.data.frame(stringsAsFactors = FALSE) %>%
+                `colnames<-`(toupper(names(.))) %>%
+                dplyr::distinct(PATID, EVTID) %>%
+                dplyr::mutate(CONDITION = query_name)
+        }, error = function(e) {
+            message(paste("Skipping", query_name, "due to error:", e$message))
+            return(NULL)
+        })
+
         res %>%
-          apply(2, unlist) %>%
-          as.data.frame(stringsAsFactors = FALSE) %>%
-          `colnames<-`(toupper(names(.))) %>%
-          dplyr::distinct(PATID, EVTID) %>%
-          dplyr::mutate(CONDITION = query_name)
-      },
-      error = function(e) {
-        message(paste("Skipping", query_name, "due to error:", e$message))
-        return(NULL)
-      }
-    )
+            dplyr::mutate(value = 1) %>%
+            tidyr::pivot_wider(names_from = CONDITION, values_from = value, values_fill = 0)
+    })
 
-    res %>%
-      dplyr::mutate(value = 1) %>%
-      tidyr::pivot_wider(names_from = CONDITION, values_from = value, values_fill = 0)
-  })
-
-  return(results)
+    return(results)
 }
