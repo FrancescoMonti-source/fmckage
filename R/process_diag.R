710a40dfb66485023c6476013cd59f767ea44c5d
Francesco Monti <58150610+FrancescoMonti-source@users.noreply.github.com>
2025-11-03 13:26:11 +0100

    ghh

--- R/process_diag.R
+++ R/process_diag.R
@@ -9,14 +9,25 @@
 #' data <- data.frame() # Example data
 #' diag_data <- process_diag(data)
 process_diag <- function(data) {
-  data %>%
-    select(ends_with("ID"), DATENT, DATSORT, DALL) %>%
-    separate_rows(DALL, sep = " ") %>%
-    mutate(
-      DATENT = as_date(DATENT),
-      DATSORT = as_date(DATSORT)
-    ) %>%
-    filter(str_detect(DALL, "\\d")) %>%
-    mutate(diag = str_extract(DALL, ":(.+)", group = 1), type_diag = str_sub(DALL, 1, 2)) %>%
-    select(-DALL)
+    data %>%
+        select(ends_with("ID"), DATENT, DATSORT, DALL) %>%
+        separate_rows(DALL, sep = " ") %>%
+        mutate(
+            DATENT  = lubridate::as_datetime(DATENT),
+            DATSORT = lubridate::as_datetime(DATSORT),
+            HEURE_DATENT   = lubridate::hour(DATENT),
+            MINUTE_DATENT  = lubridate::minute(DATENT),
+            SECOND_DATENT  = lubridate::second(DATENT),
+            HEURE_DATSORT  = lubridate::hour(DATSORT),
+            MINUTE_DATSORT = lubridate::minute(DATSORT),
+            SECOND_DATSORT = lubridate::second(DATSORT)
+        ) %>%
+        dplyr::filter(stringr::str_detect(DALL, "\\d")) %>%
+        mutate(
+            HEURE_DATENT  = hms::hms(hours = HEURE_DATENT,  minutes = MINUTE_DATENT,  seconds = SECOND_DATENT),
+            HEURE_DATSORT = hms::hms(hours = HEURE_DATSORT, minutes = MINUTE_DATSORT, seconds = SECOND_DATSORT),
+            diag      = stringr::str_extract(DALL, ":(.+)", group = 1),
+            type_diag = stringr::str_sub(DALL, 1, 2)
+        ) %>%
+        select(-DALL, -contains("MINUTE"), -contains("SECOND"))
 }
